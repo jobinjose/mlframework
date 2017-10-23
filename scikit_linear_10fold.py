74c926c1a3f80ac2edbae204a64925f41b071bc6
meenutresa <meenuvincent1992@gmail.com>
2017-10-23 19:24:38 +0100

    cross validation

--- scikit_linear_10fold.py
+++ scikit_linear_10fold.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn import datasets, linear_model
+from sklearn.model_selection import KFold
 #with open('U:/Machine learning/Assignment/Task2/House Prices/housing dataset.csv', newline='') as csvfile:
 #	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 #	for row in spamreader:
@@ -18,22 +19,31 @@ x=data.drop('SalePrice',axis = 1)
 #set the dependent variable which is saleprice
 y=data['SalePrice']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=.30)
+#x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=.30)
 
 #linear regression object creation
-linear_reg = linear_model.LinearRegression()
+kf = KFold(len(x), n_folds=10)
+xval_err = 0
 
+linear_reg = linear_model.LinearRegression()
+for train,test in kf:
+    linear_reg.fit(x[test],y[test])
+    y_pred = linear_reg.predict(x_test)
+    e=y_pred - y[test]
+    xval_err +=np.dot(e,e)
 #train the model
-linear_reg.fit(x_train,y_train)
+
 
 # test set predictions
-y_pred = linear_reg.predict(x_test)
 
+rmse_10cv = np.sqrt(xval_err/len(x))
 
 # Metrics
 #Coefficient
 print('Coefficients: \n', linear_reg.coef_)
 # The mean squared error
-print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+#print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
 # Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % r2_score(y_test, y_pred))
+#print('Variance score: %.2f' % r2_score(y_test, y_pred))
+
+print("RMSE on 10 fold: %.2f" %rmse_10cv)
