4c9df4ece89e6a6e8c1f87e6b457a6f67929434d
meenutresa <meenuvincent1992@gmail.com>
2017-10-26 01:27:18 +0100

    aded iteration nos

--- TensorFlow_linear_10fold.py
+++ TensorFlow_linear_10fold.py
@@ -35,9 +35,11 @@ error = 0
 rmse = []
 r2 = []
 rmse_10fold = 0
+i=0
 
 for train, test in kf.split(x_data):
-    print("abc")
+    i+=1
+    print("Iteration : ", i)
     x_train_array = np.asarray(x_data.iloc[train].values.tolist())
 
     #print(x_train_array)
